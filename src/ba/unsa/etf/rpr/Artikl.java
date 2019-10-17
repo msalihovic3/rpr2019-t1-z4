e678fb6348a4da3e03f360327a865b208116b806
Mirnesa Salihovic <msalihovic3@etf.unsa.ba>
2019-10-17 16:44:42 +0200

    verzija 1

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String ime;
+    private int cijena;
+    private String kod;
+
+    public Artikl(String ime, int cijena, String kod ){
+        this.ime=ime; this.cijena=cijena; this.kod=kod;
+    }
+
+    public String getNaziv() {
+        return this.ime;
+    }
+    public int getCijena(){
+        return this.cijena;
+    }
+    public String getKod(){
+        return this.kod;
+    }
 }
--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,53 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private final int max=50;
+    private Artikl [] artikli;
+    private int br_artikala;
+
+    public Korpa(){
+        br_artikala=0;
+        artikli=new Artikl[50];
+
+    }
+    public boolean dodajArtikl(Artikl a){
+        if(br_artikala<max){
+            artikli[br_artikala]=a;
+            br_artikala++;
+            return true;
+        }
+        return false;
+
+
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl pom;
+        for(int i=0; i<br_artikala; i++){
+            if(artikli[i].getKod()==kod){
+                pom=artikli[i];
+                for(int j=i; j<br_artikala-1; j++ ){
+                    artikli[j]=artikli[j+1];
+                }
+                br_artikala--;
+                return pom;
+            }
+
+        }
+
+
+
+    }
+    public Artikl[] getArtikli(){
+        return artikli;
+
+    }
+    public int dajUkupnuCijenuArtikala(){
+        int suma=0;
+        for(int i=0; i<br_artikala; i++){
+            suma += artikli[i].getCijena();
+        }
+        return  suma;
+
+    }
+
 }
